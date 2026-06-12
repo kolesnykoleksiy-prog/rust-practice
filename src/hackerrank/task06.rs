bcc62f06f2e5aa62308af6b2ad6900aa12c18ac8
kolesnykoleksiy-prog <kolesnyk.oleksiy@e-u.edu.ua>
2026-06-12 09:55:16 +0300

    Update task06.rs

--- src/hackerrank/task06.rs
+++ src/hackerrank/task06.rs
@@ -1,1 +1,63 @@
-// https://www.hackerrank.com/challenges/kangaroo/problem
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead, Write};
+
+/*
+ * Complete the 'getTotalX' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY a
+ *  2. INTEGER_ARRAY b
+ */
+
+fn getTotalX(a: &[i32], b: &[i32]) -> i32 {
+    let mut count = 0;
+
+    let start = *a.iter().max().unwrap();
+    let end = *b.iter().min().unwrap();
+
+    for x in start..=end {
+        let valid_for_a = a.iter().all(|&num| x % num == 0);
+        let valid_for_b = b.iter().all(|&num| num % x == 0);
+
+        if valid_for_a && valid_for_b {
+            count += 1;
+        }
+    }
+
+    count
+
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let mut stdin_iterator = stdin.lock().lines();
+
+    let mut fptr = File::create(env::var("OUTPUT_PATH").unwrap()).unwrap();
+
+    let first_multiple_input: Vec<String> = stdin_iterator.next().unwrap().unwrap()
+        .split(' ')
+        .map(|s| s.to_string())
+        .collect();
+
+    let n = first_multiple_input[0].trim().parse::<i32>().unwrap();
+
+    let m = first_multiple_input[1].trim().parse::<i32>().unwrap();
+
+    let arr: Vec<i32> = stdin_iterator.next().unwrap().unwrap()
+        .trim_end()
+        .split(' ')
+        .map(|s| s.to_string().parse::<i32>().unwrap())
+        .collect();
+
+    let brr: Vec<i32> = stdin_iterator.next().unwrap().unwrap()
+        .trim_end()
+        .split(' ')
+        .map(|s| s.to_string().parse::<i32>().unwrap())
+        .collect();
+
+    let total = getTotalX(&arr, &brr);
+
+    writeln!(&mut fptr, "{}", total).ok();
+}
