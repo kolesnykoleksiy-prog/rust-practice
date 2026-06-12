7e12e4caff3954ad421135b0d4a3d79157d21af0
kolesnykoleksiy-prog <kolesnyk.oleksiy@e-u.edu.ua>
2026-06-12 10:06:49 +0300

    Update task08.rs

--- src/hackerrank/task08.rs
+++ src/hackerrank/task08.rs
@@ -1,1 +1,42 @@
-// https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem
+use std::collections::HashMap;
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead, Write};
+
+fn sockMerchant(_n: i32, ar: &[i32]) -> i32 {
+    let mut counts = HashMap::new();
+
+    for &sock in ar {
+        *counts.entry(sock).or_insert(0) += 1;
+    }
+
+    counts.values().map(|&count| count / 2).sum()
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let mut stdin_iterator = stdin.lock().lines();
+
+    let mut fptr = File::create(env::var("OUTPUT_PATH").unwrap()).unwrap();
+
+    let n = stdin_iterator
+        .next()
+        .unwrap()
+        .unwrap()
+        .trim()
+        .parse::<i32>()
+        .unwrap();
+
+    let ar: Vec<i32> = stdin_iterator
+        .next()
+        .unwrap()
+        .unwrap()
+        .trim_end()
+        .split(' ')
+        .map(|s| s.parse::<i32>().unwrap())
+        .collect();
+
+    let result = sockMerchant(n, &ar);
+
+    writeln!(&mut fptr, "{}", result).ok();
+}
